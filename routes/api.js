b8bb4f0669e901ba02a347ff369ef0d294df5d63
Henrik Fricke <henrik.fricke@jimdo.com>
2015-12-01 23:54:54 +0100

    feat(heroku local): trying to use heroku postgres

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@
 // ########################################
 
 var express = require('express')
+var pg = require('pg')
 var route = express.Router()
 
 route.get('/', function(req, res, next) {
@@ -10,7 +11,21 @@ route.get('/', function(req, res, next) {
 })
 
 route.get('/kfz', function(req, res, next) {
-  res.send('kfz')
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err)
+    }
+    // client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+    //   //call `done()` to release the client back to the pool
+    //   done();
+    //
+    //   if(err) {
+    //     return console.error('error running query', err);
+    //   }
+    //   console.log(result.rows[0].number);
+    //   //output: 1
+    // });
+  })
 })
 
 module.exports = route
