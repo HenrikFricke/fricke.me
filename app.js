4b92094fa3a27f19966519f6c0c550089ee65f6e
Henrik Fricke <henrik.fricke@jimdo.com>
2015-12-01 19:56:13 +0100

    refactor(project): write routes with express js

---
+++ app.js
@@ -0,0 +1,34 @@
+// ########################################
+// ## Dependencies
+// ########################################
+
+var jade = require('jade')
+var express = require('express')
+
+// ########################################
+// ## Internal dependencies
+// ########################################
+
+var config = require('./config/config')
+
+// ########################################
+// ## Routing
+// ########################################
+
+var app = express()
+
+app.get('/*', function(req, res){
+  var page = req.url
+  if (page == "/")
+    page = '/index'
+  res.writeHead(200, {"Content-Type": "text/html"})
+  jade.renderFile( config.paths.content + page + '.jade', function (err, data) {
+    if (err)
+      res.write('Something went wrong! Try again later.')
+    else
+      res.write(data)
+  })
+  res.end()
+})
+
+app.listen(8080)
