57c245fbfa0f3d588f2b97fcfad9c4b9ea6e476c
Henrik Fricke <henrik.fricke@jimdo.com>
2015-12-03 11:52:08 +0100

    feat(api): add route for provider

--- api/api.js
+++ api/api.js
@@ -4,10 +4,12 @@
 
 var express = require('express')
 var route_atm = require('./atm.js')
+var route_provider = require('./provider.js')
 
 var route = express.Router()
 
 // all routes for api
 route.use('/atm', route_atm)
+route.use('/provider', route_provider)
 
 module.exports = route
