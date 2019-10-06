079b55b08700ab5da712c61437d2bd2983e88cad
Henrik Fricke <henrik.fricke@jimdo.com>
2019-10-06 14:03:02 +0200

    Remove Gatsby brand

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `fricke.me`,
     description: `Dev blog`,
-    author: `@HenrikFricke`
+    author: `@henrik_fricke`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -31,13 +31,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `fricke.me`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        display: `minimal-ui`
       }
     },
     `gatsby-plugin-typescript`,
